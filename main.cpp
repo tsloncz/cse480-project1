93bcb19f4976c31487810e269b1037f14d18cd50
Joshua Curl <curljosh@msu.edu>
2014-03-12 17:49:49 -0400

    Started sort merge join, results not yet accurate

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <sstream>
 #include <vector>
 #include <cstdlib>
+#include <cstring>
 using namespace std;
 
 const int block_size = 3;
@@ -29,7 +30,7 @@ int main()
 		cout << "Unable to create file out.txt" << endl;
 	}
 
-	nested_loop_join(a, b, out);
+	sort_merge_join(a, b, out);
 	a.close();
 	b.close();
 	out.close();
@@ -38,8 +39,10 @@ int main()
 
 void nested_loop_join(ifstream &a, ifstream &b, ofstream &out)
 {
-        char a_block[block_size];
-        char b_block[block_size];
+        char a_block[block_size + 1];
+        char b_block[block_size + 1];
+        a_block[block_size] = '\0';
+        b_block[block_size] = '\0';
 
 	while(a.good())
 	{
@@ -58,7 +61,7 @@ void nested_loop_join(ifstream &a, ifstream &b, ofstream &out)
                                 break;
                             }
                         }
-                        if(eq && a.good() && b.good())
+                        if(strcmp(a_block, b_block) == 0 && a.good() && b.good())
 			{
 				out.write(a_block, block_size);
                                 out << endl;
@@ -68,4 +71,44 @@ void nested_loop_join(ifstream &a, ifstream &b, ofstream &out)
 		b.seekg(0);
 	}
 	return;
+}
+
+// Assume files are sorted for now
+void sort_merge_join(ifstream &a, ifstream &b, ofstream &out)
+{
+    char a_block[block_size + 1];
+    char b_block[block_size + 1];
+    a_block[block_size] = '\0';
+    b_block[block_size] = '\0';
+    int i = 0;
+    int j = 0;
+    while(a.good() && b.good())
+    {
+        a.read(a_block, block_size);
+        a.ignore(1);
+        b.read(b_block, block_size);
+        b.ignore(1);
+
+        int a_number = atoi(a_block);
+        int b_number = atoi(b_block);       
+        if(a.good() && b.good())
+        {
+            if(a_number == b_number)
+            {
+                out.write(a_block, block_size);
+                out << endl;
+                i++;
+                j++;
+            }
+            else if(a_number < b_number)
+            {
+                i++;
+            }
+            else if(a_number > b_number)
+            {
+                j++;
+            }
+        }
+    }
+    return;
 }
